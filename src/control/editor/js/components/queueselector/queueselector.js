5005e565d8053d5f59354c4a2d7e23004008efd2
Joshua Jenkins <jenkinsj@dhbw-loerrach.de>
2020-05-31 23:46:43 +0200

    Verkleinerung des Markup-Fensters

--- src/control/editor/js/components/queueselector/queueselector.js
+++ src/control/editor/js/components/queueselector/queueselector.js
@@ -217,7 +217,7 @@ class QueueSelector {
 				null,
 				() => {
 					this.state.queue.selected = false;
-					this.select.set_button_html('Stapel');
+					this.select.set_button_html('Anzeigestapel');
 					this.update();
 				},
 				null
